dfa552b57ca2b72107abfad3526281def75a1318
Mark Cooper <markchristophercooper@gmail.com>
2018-05-25 17:41:59 -0700

    Begin defining site

--- processor/model/site.rb
+++ processor/model/site.rb
@@ -1,1 +1,18 @@
 # SITE
+class Site
+  include Dynamoid::Document
+
+  table :name           => :sites,
+        :key            => :site_id,
+        :read_capacity  => 1,
+        :write_capacity => 1
+
+  field :site
+  field :manifest
+  field :name
+  field :contact
+  field :email
+  field :timezone
+  field :username
+  field :password
+end
