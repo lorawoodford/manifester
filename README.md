6ea98da7be5bdd813082eebe5d5c852ea42e0cb1
Mark Cooper <markchristophercooper@gmail.com>
2018-05-25 17:51:42 -0700

    Scope envvars with app name (for now)

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ A manifest processor and api endpoint for archival resources.
 
 ## Setup
 
-Set environment variables for `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`.
+Set environment variables for:
+
+- `MANIFESTER_AWS_ACCESS_KEY_ID`
+- `MANIFESTER_AWS_SECRET_ACCESS_KEY`
--- processor/config/db.rb
+++ processor/config/db.rb
@@ -2,9 +2,9 @@ require 'aws-sdk'
 require 'dynamoid'
 
 Dynamoid.configure do |config|
-  config.access_key = ENV.fetch('AWS_ACCESS_KEY_ID')
-  config.secret_key = ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  config.region     = ENV.fetch('AWS_REGION', 'us-west-2')
+  config.access_key = ENV.fetch('MANIFESTER_AWS_ACCESS_KEY_ID')
+  config.secret_key = ENV.fetch('MANIFESTER_AWS_SECRET_ACCESS_KEY')
+  config.region     = ENV.fetch('MANIFESTER_AWS_REGION', 'us-west-2')
   config.namespace  = "manifester_#{ENV.fetch('MANIFESTER_ENV', 'development')}"
 end
 
